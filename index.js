1f8208c722ba0ad45ba958464c92d05d33b7b0c3
nylon <WoW@MvM.com>
2023-11-09 05:15:27 +0800

    進場階段完成

--- index.js
+++ index.js
@@ -7,4 +7,14 @@ $(function(){
   setTimeout(function(){
     $('#obj').addClass('active');
   }, 3000);
+  // }, 0);
+
+  setTimeout(function(){
+    $('#char').addClass('active');
+  }, 4000);
+
+  setTimeout(function(){
+    $('#name').addClass('active');
+  }, 5000);
+  // }, 1000);
 });
