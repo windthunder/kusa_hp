39c296ccd0fda4b93b5608fc37c709a4bd2c733f
nylon <WoW@MvM.com>
2023-11-13 01:24:21 +0800

    meow

--- index.js
+++ index.js
@@ -1,20 +1,35 @@
-$(function(){
+$(window).on('load', function(){
+  setTimeout(function(){
+    $('.logo').addClass('aaa').removeClass('bbb');
+  }, 500);
+
+  setTimeout(function(){
+    $('.logo').removeClass('aaa').addClass('bbb');
+  }, 2000);
+
   setTimeout(function(){
     $('.door').addClass('active');
-  }, 1000);
-  // }, 0);
+  // }, 500);
+  }, 3500);
 
   setTimeout(function(){
-    $('#obj').addClass('active');
-  }, 3000);
-  // }, 0);
+    $('.obj').addClass('active');
+  }, 5500);
 
   setTimeout(function(){
-    $('#char').addClass('active');
-  }, 4000);
+    $('.char').addClass('active');
+  }, 6500);
 
   setTimeout(function(){
     $('#name').addClass('active');
-  }, 5000);
-  // }, 1000);
+  }, 7500);
+
+  setTimeout(function(){
+    $('.door2').addClass('active');
+  }, 8500);
+
+
+});
+
+$(function(){
 });
